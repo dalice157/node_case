9c247ea24f2f6fd0f4c87e90b2fa4f83ac701240
dalice.chu <dalice.chu@104.com.tw>
2018-03-13 11:15:29 +0800

    將fs直接module.exports出來

--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ var app = express();
 var path = require('path');
 var routes = require('../routes.js')(app);
 var port = 8000;
+var wrFile = require('../src/wrFile').write;
 
-require('../src/wrFile')
+app.use(express.static(path.join('public'))); /* 將預設路徑設在public*/
 
-app.use(express.static(path.join('public')));/* 將預設路徑設在public*/
 
 app.get('/', function (req, res) { //靜態網頁
   res.sendFile('index.html');
+});
+
+
+app.get('/test', function (req, res) {
+  res.send(require('../public/test.json'));
 });
 
 app.listen(port,() => console.log(`listening on ${port}`));
--- src/wrFile.js
+++ src/wrFile.js
@@ -2,7 +2,17 @@ var fs = require('fs');
 var api = require('../api/db');
 var data = JSON.stringify(api);
 
-fs.writeFile('./public/test.json', data, function (err) {
-	if (err) throw err;
-	console.log("嘿嘿", data);
-});
+// fs.writeFile('./public/test.json', data, function (err) {
+// 	if (err) throw err;
+// })
+
+module.exports  = {
+  write: fs.writeFile('./public/test.json', data, function (err) {
+		if (err) throw err;
+		fs.readFile('./public/test.json', function (err, dataFile) {
+      if (err) {
+         return console.error(err);
+      }
+	 	});
+	})
+};
